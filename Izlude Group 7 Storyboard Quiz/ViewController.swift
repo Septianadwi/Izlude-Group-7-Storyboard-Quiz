052a83cd8d36d93975165d72f0b0eba1139e8c59
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-07-22 23:59:06 +0700

    Init project with asset

---
+++ Izlude Group 7 Storyboard Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Izlude Group 7 Storyboard Quiz
+//
+//  Created by PT.Koanba on 22/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
